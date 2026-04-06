26303cddf9da48b7235ccd68d65ac3c50188ee7e
abdullah-fz <abdullahalzahim1@gmail.com>
2026-04-06 09:08:25 +0300

    Solve TODO 3: implement getRandomQuote

--- 6-3-node-express/backend/quotes.js
+++ 6-3-node-express/backend/quotes.js
@@ -1,4 +1,4 @@
-import { getRandomInt } from "./utils/random.js";
+// Import random.js file
 
 const quotes = [
   "Code is like humor. When you have to explain it, it’s bad.",
@@ -7,7 +7,9 @@ const quotes = [
   "Programming isn’t about what you know; it’s about what you can figure out."
 ];
 
-// TODO 3: implement getRandomQuote
+// TODO 3: Create and export getRandomQuote function
+//Hint: export function getRandomQuote(){}
+
 export function getRandomQuote() {
   const index = getRandomInt(quotes.length);
   return quotes[index];
